b5c9fcb0ae101ae162ae03f2e73ac68b6d27b1c1
PaulPintang <paulpintang.dev@gmail.com>
2023-04-06 14:53:04 +0800

    preview

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ alias wifiConn="nmcli d"
 alias lf="ranger"
 alias update="sudo pacman -Syyu && yay -Syu"
 alias logout="pkill -u paul"
+alias s="startx"
 #alias ls='nnn -de'
 alias hotspot="sudo sh create_ap wlp2s0 wlp2s0 arco mypass..."
 alias connectonefist="sudo killall openvpn; sudo openvpn --config OVPN/OneFistConfig/file.ovpn --auth-user-pass OVPN/OneFistConfig/pass.txt"
