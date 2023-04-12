1d39f6b21ee07e0c2e78f624e8f857f161b54ff2
PaulPintang <paulpintang.dev@gmail.com>
2023-04-12 18:36:15 +0800

    mtpfs script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,10 @@ alias lf="ranger"
 alias update="sudo pacman -Syyu && yay -Syu"
 alias logout="pkill -u paul"
 alias s="startx"
-#alias ls='nnn -de'
+alias mtp='~/.local/bin/dmtpfs.sh'
 alias hotspot="sudo sh create_ap wlp2s0 wlp2s0 arco mypass..."
-alias connectonefist="sudo killall openvpn; sudo openvpn --config OVPN/OneFistConfig/file.ovpn --auth-user-pass OVPN/OneFistConfig/pass.txt"
-alias connectsuwen="sudo killall openvpn; sudo openvpn --config OVPN/SuwenConfig/file.ovpn --auth-user-pass OVPN/SuwenConfig/pass.txt"
+alias connectonefist="sudo killall openvpn; sudo openvpn --config ~/.ovpn/onefist/file.ovpn --auth-user-pass ~/.ovpn/onefist/pass.txt"
+alias connectsuwen="sudo killall openvpn; sudo openvpn --config ~/.ovpn/suwen/file.ovpn --auth-user-pass ~/.ovpn/suwen/pass.txt"
 
 export NNN_FCOLORS='c1e28d2e006033f7c6d6abc4'
 
---
+++ .local/bin/dmtpfs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+Dir=/mnt/phone/
+
+# Select device
+Devices=$(simple-mtpfs -l)
+
+if [ ! -n "$Devices" ]; then
+	notify-send "No devices found"
+	exit
+else
+  Device=$(echo "$Devices" | dmenu -p "Select device:" || notify-send "No device selected")
+  Id=${Device%%:*}
+  Name=${Device##*: }
+	if [ ! -d "$Dir$Name" ]; then
+		mkdir "$Dir$Name"
+	fi
+fi
+
+if [ ! -n "$(find "$Dir$Name" -maxdepth 0 -empty)" ]; then
+  fusermount -u "$Dir$Name" && notify-send "Android Mount" "Device Unmounted"
+else
+  simple-mtpfs --device "$Id" "$Dir$Name" -o allow_root && notify-send "Android Mount" "Device Mounted in $Dir$Name"
+fi
