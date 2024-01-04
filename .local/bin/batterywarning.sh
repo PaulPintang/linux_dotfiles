d9de1279b0fede135bc5770eba52104733869c6f
PaulPintang <paulpintang.dev@gmail.com>
2024-01-04 20:02:11 +0800

    remove alert audio

--- .local/bin/batterywarning.sh
+++ .local/bin/batterywarning.sh
@@ -8,10 +8,8 @@ do
 
     if [[ "$BATTERY_PERCENTAGE" -le "20" && $CABLE = "no" ]]; then
         notify-send -i lowbatt "WARNING: Low Battery" "$BATTERY_PERCENTAGE% of battery remaining!" -u critical -r 9991 -t 8000;
-        paplay /home/paul/.local/bin/soundalerts/low.wav
     elif [[ "$BATTERY_PERCENTAGE" -ge "98" && $CABLE = "yes" ]]; then
         notify-send -i fullbatt "Battery Full: 100%" "Please disconnect your charger!" -u low -r 9991 -t 8000 
-      	paplay /home/paul/.local/bin/soundalerts/full.wav
     fi
 sleep 1m
 done
