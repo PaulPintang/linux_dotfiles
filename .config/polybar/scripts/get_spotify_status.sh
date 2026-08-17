83a80a6ab56c266722dab31579131e2eec3cd96a
PaulPintang <paulpintang.dev@gmail.com>
2026-08-17 21:31:34 +0800

    update polybar spotify script and vscode custom css

--- .config/polybar/scripts/get_spotify_status.sh
+++ .config/polybar/scripts/get_spotify_status.sh
@@ -5,46 +5,55 @@ PARENT_BAR="main"
 PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 
 # Set the source audio player here.
-# Players supporting the MPRIS spec are supported.
-# Examples: spotify, vlc, chrome, mpv and others.
-# Use `playerctld` to always detect the latest player.
-# See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
 PLAYER="spotify"
 
 # Format of the information displayed
-# Eg. {{ artist }} - {{ album }} - {{ title }}
-# See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
 FORMAT="{{ title }} - {{ artist }}"
 
 # Sends $2 as message to all polybar PIDs that are part of $1
 update_hooks() {
     while IFS= read -r id
     do
-        polybar-msg -p "$id" hook spotify-play-pause $2 1>/dev/null 2>&1
+        polybar-msg -p "$id" hook spotify-play-pause "$2" >/dev/null 2>&1
     done < <(echo "$1")
 }
 
-PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
+# Get Spotify status
+PLAYERCTL_STATUS=$(playerctl --player="$PLAYER" status 2>/dev/null)
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -eq 0 ]; then
-    STATUS=$PLAYERCTL_STATUS
+    STATUS="$PLAYERCTL_STATUS"
 else
-    STATUS="Spotify"
+    STATUS="No player is running"
 fi
 
 if [ "$1" == "--status" ]; then
     echo "$STATUS"
+    exit 0
+fi
+
+if [ "$STATUS" = "Playing" ]; then
+    # PLAYING → show PAUSE icon
+    update_hooks "$PARENT_BAR_PID" 1
+
+    playerctl --player="$PLAYER" metadata --format "$FORMAT" 2>/dev/null || echo "Spotify"
+
+elif [ "$STATUS" = "Paused" ]; then
+    # PAUSED → show PLAY icon
+    update_hooks "$PARENT_BAR_PID" 2
+
+    playerctl --player="$PLAYER" metadata --format "$FORMAT" 2>/dev/null
+
+elif [ "$STATUS" = "Stopped" ]; then
+    # STOPPED → show PLAY icon
+    update_hooks "$PARENT_BAR_PID" 2
+
+    echo "No music is playing"
+
 else
-    if [ "$STATUS" = "Stopped" ]; then
-        echo "No music is playing"
-    elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
-        playerctl --player=$PLAYER metadata --format "$FORMAT"
-    elif [ "$STATUS" = "No player is running"  ]; then
-        echo "$STATUS"
-    else
-        update_hooks "$PARENT_BAR_PID" 1
-        playerctl --player=$PLAYER metadata --format "$FORMAT" 2>/dev/null || echo "Spotify"
-    fi
+    # NO SPOTIFY → show PLAY icon
+    update_hooks "$PARENT_BAR_PID" 2
+
+    echo "Spotify"
 fi
